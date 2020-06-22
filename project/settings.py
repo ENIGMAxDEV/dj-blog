aebf11ef30ce9ab665a7e3337e976be41b537a91
ENIGMAxDEV <enigmaxdev@gmail.com>
2020-06-22 23:37:37 +0300

    update

--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # my app
+    'blog',
+    'user',
 ]
 
 ROOT_URLCONF = 'project.urls'
